cb436892e00d2698bb3c139affa523e2b5936fa1
maxmelnyk1311 <141562499+maxmelnyk1311@users.noreply.github.com>
2024-01-15 19:24:48 +0000

    Deploying to gh-pages from @ maxmelnyk1311/goit-js-hw-11@2fdf970c4c32a8df2bad961c8f511238e84a2397 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,25 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import{f as h,i as y}from"./assets/vendor-651d7991.js";const f=document.querySelector('input[type="text"]'),n=document.querySelector("[data-start]");n.disabled=!0;const p=document.querySelector("[data-days]"),C=document.querySelector("[data-hours]"),g=document.querySelector("[data-minutes]"),S=document.querySelector("[data-seconds]");let r;const q={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(s){r=s[0];const e=Date.now();r.getTime()<=e?(n.disabled=!0,y.warning({message:"Please choose date in the future",position:"topRight",backgroundColor:"red"})):n.disabled=!1}};h(f,q);n.addEventListener("click",s=>{s.preventDefault();let e=r-Date.now();function u(t){const d=Math.floor(t/864e5),i=Math.floor(t%864e5/36e5),l=Math.floor(t%864e5%36e5/6e4),m=Math.floor(t%864e5%36e5%6e4/1e3);return{days:d,hours:i,minutes:l,seconds:m}}const c=setInterval(()=>{e-=1e3,console.log(e/1e3);let t=u(e);function o(a){return String(a).padStart(2,"0")}p.textContent=o(t.days),C.textContent=o(t.hours),g.textContent=o(t.minutes),S.textContent=o(t.seconds),e/1e3<1&&clearInterval(c)},1e3)});
+import{i,S as u}from"./assets/vendor-9310f15c.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const l of e.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&o(l)}).observe(document,{childList:!0,subtree:!0});function s(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerpolicy&&(e.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?e.credentials="include":t.crossorigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function o(t){if(t.ep)return;t.ep=!0;const e=s(t);fetch(t.href,e)}})();const f=document.querySelector(".form"),c=document.querySelector(".form-input"),p=document.querySelector(".gallery"),a=document.querySelector(".css-loader");a.style.display="none";const m=new URLSearchParams({key:"41748300-cb29be2b9e713bfb06ce55aab",q:"",image_type:"photo",orientation:"horizontal",safesearch:"true"});f.addEventListener("submit",n=>{n.preventDefault(),a.style.display="block",c.value.trim().length!==0&&(m.set("q",c.value.trim()),fetch(`https://pixabay.com/api/?${m}`).then(r=>{if(!r.ok)throw new Error("error");return r.json()}).then(({hits:r})=>{r.length===0&&i.error({position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"});const s=r.reduce((t,e)=>t+`<li class="gallery-item">
+                            <a class="gallery-link" href="${e.largeImageURL}">
+                                <img 
+                                    class="gallery-image" 
+                                    data-source=${e.largeImageURL} 
+                                    src=${e.webformatURL} 
+                                    alt=${e.tags} 
+                                />
+                            </a>
+                            <div class="gallery-item-info">
+                                <p class="gallery-item-info-name">Likes
+                                    <span class="gallery-item-info-value">${e.likes}
+                                </p>
+                                <p class="gallery-item-info-name">Views
+                                    <span class="gallery-item-info-value">${e.views}</span>
+                                </p>
+                                <p class="gallery-item-info-name">Comments
+                                    <span class="gallery-item-info-value">${e.comments}</span>
+                                </p>
+                                <p class="gallery-item-info-name">Downloads
+                                    <span class="gallery-item-info-value">${e.downloads}</span>
+                                </p>
+                            </div>
+                        </li>`,"");p.innerHTML=s,a.style.display="none";let o=new u(".gallery a",{captionDelay:250,captionsData:"alt"});o.refresh(),o.on("show.simplelightbox",function(){})}).catch(()=>{i.error({position:"topRight",message:"Something is wrong!"})}))});
 //# sourceMappingURL=commonHelpers.js.map
